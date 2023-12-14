3aed5223653a4307236e2be9853e7ba138ce589d
Thomas Skarshaug <skarshaug.thomas@gmail.com>
2023-12-14 22:03:44 +0100

    DLL loading

--- src/main.zig
+++ src/main.zig
@@ -1,24 +1,41 @@
 const std = @import("std");
+const windows = std.os.windows;
+const builtin = @import("builtin");
 
-pub fn main() !void {
-    // Prints to stderr (it's a shortcut based on `std.io.getStdErr()`)
-    std.debug.print("All your {s} are belong to us.\n", .{"codebase"});
+const Foo = @import("root.zig").Foo;
 
-    // stdout is for the actual output of your application, for example if you
-    // are implementing gzip, then only the compressed bytes should be sent to
-    // stdout, not any debugging messages.
-    const stdout_file = std.io.getStdOut().writer();
-    var bw = std.io.bufferedWriter(stdout_file);
-    const stdout = bw.writer();
+// Windows
+extern "kernel32" fn GetProcAddress(h_module: windows.HMODULE, fn_name: windows.LPCSTR) callconv(windows.WINAPI) ?windows.FARPROC;
+extern "kernel32" fn LoadLibraryA(fn_name: windows.LPCSTR) callconv(windows.WINAPI) ?windows.HMODULE;
 
-    try stdout.print("Run `zig build test` to run the tests.\n", .{});
+var add: *const fn (i32, i32) i32 = undefined;
+var makeFoo: *const fn (*std.mem.Allocator, i32, i32) *Foo = undefined;
 
-    try bw.flush(); // don't forget to flush!
+fn win32LoadDllFunctionPointers() void {
+    const dll = LoadLibraryA("zig-out/lib/zig-dll-demo.dll");
+    if (dll != null) {
+        add = @as(@TypeOf(add), @ptrCast(@alignCast(GetProcAddress(dll.?, "add"))));
+        makeFoo = @as(@TypeOf(makeFoo), @ptrCast(@alignCast(GetProcAddress(dll.?, "makeFoo"))));
+    }
 }
 
-test "simple test" {
-    var list = std.ArrayList(i32).init(std.testing.allocator);
-    defer list.deinit(); // try commenting this out and see if zig detects the memory leak!
-    try list.append(42);
-    try std.testing.expectEqual(@as(i32, 42), list.pop());
+fn loadDLLFunctionPointers() !void {
+    var dll_file = try std.DynLib.open("zig-out/lib/zig-dll-demo.dll");
+    add = dll_file.lookup(@TypeOf(add), "add").?;
+    makeFoo = dll_file.lookup(@TypeOf(makeFoo), "makeFoo").?;
+}
+
+pub fn main() !void {
+    var gpa = std.heap.GeneralPurposeAllocator(.{}){};
+    var allocator = gpa.allocator();
+
+    //win32LoadDllFunctionPointers();
+
+    try loadDLLFunctionPointers();
+
+    const result = add(1, 2);
+    std.debug.print("add(1,2) = {}\n", .{result});
+
+    const foo = makeFoo(&allocator, 72, 49);
+    std.debug.print("foo = {}\n", .{foo});
 }
--- src/root.zig
+++ src/root.zig
@@ -1,5 +1,31 @@
 const std = @import("std");
 const testing = std.testing;
+const Allocator = std.mem.Allocator;
+
+pub const Foo = struct {
+    allocator: *Allocator,
+    width: i32,
+    height: i32,
+    name: []const u8,
+
+    pub fn init(allocator: *Allocator, width: i32, height: i32) *Foo {
+        const foo = allocator.create(Foo) catch unreachable;
+        foo.*.allocator = allocator;
+        foo.*.width = width;
+        foo.*.height = height;
+        foo.*.name = "Foo";
+        return foo;
+    }
+
+    pub fn incrementWidth(self: *Foo) void {
+        self.width += 1;
+        self.bar.incrementId();
+    }
+};
+
+export fn makeFoo(allocator: *Allocator, width: i32, height: i32) *Foo {
+    return Foo.init(allocator, width, height);
+}
 
 export fn add(a: i32, b: i32) i32 {
     return a + b;
